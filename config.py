e5d5c1b6d31a150c1ff64d0d9a5e4147aca1df82
Perova28 <sashulka-sch@mail.ru>
2022-05-27 23:03:09 +0300

    working telegram bot

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = '5391839250:AAGrRliZ_BMvkHpp_mzzm-fjuQ1IVqCL41s'
 keys = {
-    'Доллар': 'USD',
-    'Рубль': 'RUB',
-    'Евро': 'EUR'
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR'
 }
